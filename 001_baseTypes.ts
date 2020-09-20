5856f7f2303d9520581e540b86aff17cd7755cec
Carlos Martinez <carlos.martinez@biko2.com>
2020-09-20 19:15:28 +0200

    Ejercicio 1

---
+++ 001_baseTypes.ts
@@ -0,0 +1,26 @@
+const number1: number = 12
+const number2: number = 4
+const text1: string = "Hello"
+const text2: string = "World"
+
+/*
+ *  Reemplazar los undefined por los tipos correctos
+ */ 
+const resutl01: undefined = number1 + number2
+const resutl02: undefined = number1 - number2
+const resutl03: undefined = number1 * number2
+const resutl04: undefined = number1 / number2
+const resutl05: undefined = text1 + text2
+const resutl06: undefined = number1 + text1
+const result07: undefined = !!number1
+const result08: undefined = number1 === number2
+const result09: undefined = number1 !== number2
+const result10: undefined = number1 && number2
+const result11: undefined = number1 && text1
+const result12: undefined = text1 && number2
+const result13: undefined = text1 && text1
+const result15: undefined = number1 || number2
+const result16: undefined = number1 || text1
+const result18: undefined = text1 || number2
+const result19: undefined = text1 || text1
+
