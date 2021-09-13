7a04227d075ea0adf80b833ea6aeb19abdf37a77
Javier <javier.gutierrez@540deg.com>
2021-09-13 13:26:11 +0200

    Add optional chaining exercise

---
+++ Ejercicio_9.ts
@@ -0,0 +1,36 @@
+/*
+    Encadenamiento opcional u optional chaining (?.)
+    El operador ?. funciona de manera similar a el operador de encadenamiento .,
+    excepto que en lugar de causar un error si una referencia es casi-nula (null o undefined),
+    la expresión hace una evaluación de circuito corto con un valor de retorno de undefined.
+
+    Puedes saber más en: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-7.html#optional-chaining
+
+    --- Resultado esperado por consola
+    Alicia tiene perro que se llama undefined
+*/
+
+
+interface Personaje {
+    name: string
+    animal: Animal
+}
+
+interface Animal {
+    type: string
+    name?: string
+
+}
+
+const personaje: Personaje = {
+    name: 'Alicia',
+    animal: {
+        type: 'Toby'
+    }
+}
+
+// Modifica el código bajo esta línea
+const getName = (animal: Animal) => animal.name
+// Modifica el código sobre esta línea
+
+console.log(`${personaje.name} tiene un ${personaje.animal} que se llama ${getName(personaje.animal)}`)
